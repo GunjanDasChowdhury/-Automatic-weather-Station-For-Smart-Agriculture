b2fc60f4d2c5b4d10e394c23d946849451b9845a
GunjanDasChowdhury <138147210+GunjanDasChowdhury@users.noreply.github.com>
2023-06-29 22:22:50 +0530

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,35 +1,76 @@
 #include <SimpleDHT.h>
+#include <Adafruit_Sensor.h>
+#include <ThingSpeak.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
 
-// for DHT11, 
-//      VCC: 5V or 3V
-//      GND: GND
-//      DATA: 2
 int pinDHT11 = 2;
 SimpleDHT11 dht11(pinDHT11);
 
+int soilMoisturePin = A0;
+
+const char* ssid     = "Nimo";
+const char* password = "khuchrokatha";
+
+  
+  float soilMoisture = 0;
+
+
+
+  WiFiClient client;
+
+  unsigned long myChannelNumber = 1749280; //Your Channel Number (Without Brackets)
+  const char * myWriteAPIKey = "B7YA0B87ZY305HHL"; //Your Write API Key
+
+  
+
 void setup() {
   Serial.begin(115200);
-}
 
-void loop() {
-  // start working...
-  Serial.println("=================================");
-  Serial.println("Sample DHT11...");
-  
-  // read without samples.
-  byte temperature = 0;
-  byte humidity = 0;
-  int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
-    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
-    return;
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  dht11.read(&temperature, &humidity, NULL);
-  Serial.print("Sample OK: ");
-  Serial.print((int)temperature); Serial.print(" *C, "); 
-  Serial.print((int)humidity); Serial.println(" H");
   
-  // DHT11 sampling rate is 1HZ.
-  delay(1500);
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+
+  ThingSpeak.begin(client);
+  
+}
+void loop(){
+  Serial.println(WiFi.SSID());
+  
+  Serial.println("+++++++++++++++++++++++++++++");
+  byte temp = 0;
+  byte hum = 0;
+
+  dht11.read(&temp, &hum, NULL);
+  
+  
+  Serial.print((float)temp); 
+  Serial.print(" *C, "); 
+  Serial.print((float)hum); 
+  Serial.println(" H");
+  
+  soilMoisture = ( 100.10 - ( (analogRead(soilMoisturePin)/1023.00) * 100.00 ) );
+  Serial.println(soilMoisture);
+  ThingSpeak.setField(1,temp);
+  ThingSpeak.setField(2,hum);
+  ThingSpeak.setField(3,soilMoisture);
+  
+  ThingSpeak.writeFields(myChannelNumber,myWriteAPIKey); 
+
+  delay(1000);
+
+  delay(500);
+
+  Serial.println("Data Updated");
 }
